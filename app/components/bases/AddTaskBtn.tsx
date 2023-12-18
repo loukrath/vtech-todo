daebde33a9752a8f7094a67471e912c48a8873ee
munyrath <munyrath@gmail.com>
2023-12-19 00:09:30 +0700

    Create todo list modal

--- app/components/bases/AddTaskBtn.tsx
+++ app/components/bases/AddTaskBtn.tsx
@@ -1,12 +1,47 @@
+'use client'
+
 import { AiOutlinePlus } from 'react-icons/ai'
+import Modal from '@/app/components/bases/Modal'
+import { useState } from 'react'
 
 const AddTaskBtn = () => {
+  const [isShowModal, setModalOpen] = useState<boolean>(false)
+  const [newTask, setNewTask] = useState<string>('')  
+  const handleSubmitNewTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('new task', newTask)
+
+    // setModalOpen(false)
+  }
+
   return (
     <div>
-      <button className="btn btn-primary w-full text-white uppercase">
+      <button onClick={() => setModalOpen(true)} className="btn btn-primary w-full text-white uppercase">
         Add new task
         <AiOutlinePlus className='ml-1' size={16} />
       </button>
+
+      <Modal isShowModal={isShowModal} setModalOpen={setModalOpen} >
+        <form onSubmit={handleSubmitNewTask}>
+          <h3 className='font-bold text-lg text-center'>
+            Add new task
+          </h3>
+
+          <div className='modal-action'>
+            <input
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              type="text"
+              placeholder="New task"
+              className="input input-bordered w-full max-w-full"
+            />
+
+            <button type='submit' className='btn btn-primary'>
+              Add
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
           <Image className='mx-auto' src="vtech-logo.svg" alt="me" width="128" height="128" />
         </div>
 
-        <h1 className='text-center text-2xl text-primary mb-10'>
+        <h1 className='text-center text-3xl uppercase text-primary mb-10'>
           Vtech Todo List
         </h1>
 
