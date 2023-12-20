9ce1dd234f9ae290048d9e148d710852450c2d82
munyrath <munyrath@gmail.com>
2023-12-20 21:42:03 +0700

    Move modal out from list of table

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -10,49 +10,19 @@ import { updateTask, deleteTask } from '@/utils/api'
 
 
 interface TaskProps {
-  task: ITask
+  task: ITask,
+  setModalEditOpen: (open: boolean, task: ITask) => boolean | void,
+  setModalDeleteOpen: (open: boolean, id: string) => boolean | void,
 }
 
-const Task: React.FC<TaskProps> = ({ task }) => {
-  const [taskToEdit, setTaskToEdit] = useState<string>(task.title)
-
+const Task: React.FC<TaskProps> = ({ task, setModalEditOpen, setModalDeleteOpen }) => {
   // Booleans
-  const [isShowEditModal, setIsShowEditModal] = useState<boolean>(false)
-  const [isShowDeleteModale, setIsShowDeleteModale] = useState<boolean>(false)
   const [isLoadingUpdate, setIsLoadingUpdate] = useState<boolean>(false)
-  const [isLoadingDelete, setIsLoadingDelete] = useState<boolean>(false)
 
   /**
    * Functions
    */
   const router = useRouter();
-
-  // Functions for handle edit task
-  const handleUpdateTask = async (newTask: ITask) => {
-    return await updateTask(newTask)
-  }
-
-  const handleSubmitEditTask = async (e: React.FormEvent<HTMLFormElement>) => {
-    try {
-      e.preventDefault()
-      setIsLoadingUpdate(true)
-
-      const newTask = {
-        id: task.id,
-        title: taskToEdit,
-        isCompleted: task.isCompleted
-      }
-
-      await handleUpdateTask(newTask)
-
-      setIsShowEditModal(false)
-      router.refresh()
-    } catch (error) {
-      console.log('error', error)
-    } finally {
-      setIsLoadingUpdate(false)
-    }
-  }
 
   const handleUpdateTaskStatus = async () => {
     try {
@@ -63,28 +33,13 @@ const Task: React.FC<TaskProps> = ({ task }) => {
         isCompleted: !task.isCompleted
       }
   
-      await handleUpdateTask(newTask)
+      await updateTask(newTask)
   
       router.refresh()
     } catch (error) {
       console.log('error', error)
     } finally {
       setIsLoadingUpdate(false)
-    }
-  }
-
-  const handleDeleteTask = async () => {
-    try {
-      setIsLoadingDelete(true)
-
-      await deleteTask(task.id)
-
-      setIsShowDeleteModale(false)
-      router.refresh()
-    } catch (error) {
-      console.log('error', error)
-    } finally {
-      setIsLoadingDelete(false)
     }
   }
 
@@ -116,74 +71,19 @@ const Task: React.FC<TaskProps> = ({ task }) => {
           }
         </td>
         <td className="flex opacity-0 space-x-2 group-hover:opacity-100">
-          <FiEdit onClick={() => setIsShowEditModal(true)} cursor="pointer" className="text-blue-500" size={25} />
+          <FiEdit
+            onClick={() => setModalEditOpen(true, task)}
+            cursor="pointer"
+            className="text-blue-500" size={25} />
 
           <FiTrash2
-            onClick={() => setIsShowDeleteModale(true)}
+            onClick={() => setModalDeleteOpen(true, task.id)}
             cursor="pointer"
             className="text-red-500"
             size={25}
           />
         </td>
       </tr>
-
-      {/* Edit modal */}
-      <Modal
-        isShowModal={isShowEditModal}
-        setModalOpen={setIsShowEditModal}
-        >
-        <form onSubmit={handleSubmitEditTask}>
-          <h3 className='font-bold text-lg text-center text-primary'>
-            Edit task
-          </h3>
-
-          <div className='modal-action'>
-            <input
-              value={taskToEdit}
-              onChange={(e) => setTaskToEdit(e.target.value)}
-              type="text"
-              placeholder="New task"
-              className="input input-bordered w-full max-w-full bg-white"
-            />
-
-            <button type='submit' className='btn btn-primary text-white'>
-              { isLoadingUpdate && <span className="loading loading-sm"></span> }
-              Update
-            </button>
-          </div>
-        </form>
-      </Modal>
-      {/* !! Edit modal */}
-
-      {/* Delete modal */}
-      <Modal
-        isShowModal={isShowDeleteModale}
-        setModalOpen={setIsShowDeleteModale}
-      >
-        <h3 className='font-bold text-lg text-center text-primary'>
-          Are you sure you want to delete this task?
-        </h3>
-
-        <div className="modal-action">
-          <div className="space-x-2 w-full flex">
-            <button
-              onClick={() => setIsShowDeleteModale(false)}
-              className="btn btn-ghost flex-1"
-            >
-              Cancel
-            </button>
-
-            <button
-              onClick={handleDeleteTask}
-              className="btn btn-error flex-1 text-white"
-            >
-              { isLoadingDelete && <span className="loading loading-sm"></span> }
-              Confirm
-            </button>
-          </div>
-        </div>
-      </Modal>
-      {/* !! Delete modal */}
     </>
   )
 }
--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,7 +1,12 @@
-import React from "react"
+"use client"
+
+import React, { useState } from "react"
+import { useRouter } from "next/navigation"
 
 import { ITask } from "@/types/tasks"
 import Task from "@/app/components/Task"
+import Modal from "./bases/Modal"
+import { updateTask, deleteTask } from "@/utils/api"
 
 
 interface TodoListProps {
@@ -9,23 +14,143 @@ interface TodoListProps {
 }
 
 const TodoListTable: React.FC<TodoListProps> = ({ tasks }) => {
+  const router = useRouter();
+
+  const [isLoadingUpdate, setIsLoadingUpdate] = useState<boolean>(false)
+  const [isShowEditModal, setIsShowEditModal] = useState<boolean>(false)
+  const [isShowDeleteModal, setIsShowDeleteModal] = useState<boolean>(false)
+  const [isLoadingDelete, setIsLoadingDelete] = useState<boolean>(false)
+  const [taskToEdit, setTaskToEdit] = useState<ITask>({id: '', title: '', isCompleted: false})
+  const [idTaskToDelete, setIdTaskToDelete] = useState<string>('')
+
+  /**
+   * Functions
+   */
+  const handleOpenEditModal = (isOpen: boolean, task: ITask) => {
+    setTaskToEdit(task)
+    setIsShowEditModal(isOpen)
+  }
+
+  const handleSubmitEditTask = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault()
+      setIsLoadingUpdate(true)
+
+      await updateTask(taskToEdit)
+
+      setIsShowEditModal(false)
+      router.refresh()
+    } catch (error) {
+      console.log('error', error)
+    } finally {
+      setIsLoadingUpdate(false)
+    }
+  }
+
+  const handleOpenDeleteModal = (isOpen: boolean, id: string) => {
+    setIdTaskToDelete(id)
+    setIsShowDeleteModal(isOpen)
+  }
+
+  const handleDeleteTask = async () => {
+    try {
+      setIsLoadingDelete(true)
+
+      await deleteTask(idTaskToDelete)
+
+      setIsShowDeleteModal(false)
+      router.refresh()
+    } catch (error) {
+      console.log('error', error)
+    } finally {
+      setIsLoadingDelete(false)
+    }
+  }
+
   return (
-    <div className="bg-white mt-5 rounded-lg">
-      <div className="overflow-x-auto">
-        <table className="table text-black">
-          <thead>
-            <tr className="border-b-0 bg-gray-200 text-black">
-              <th>TODO</th>
-              <th className="text-center">Status</th>
-              <th>ACTION</th>
-            </tr>
-          </thead>
-          <tbody>
-            { tasks.map((task) => <Task key={task.id} task={task} />) }
-          </tbody>
-        </table>
+    <>
+      <div className="bg-white mt-5 rounded-lg">
+        <div className="overflow-x-auto">
+          <table className="table text-black">
+            <thead>
+              <tr className="border-b-0 bg-gray-200 text-black">
+                <th>TODO</th>
+                <th className="text-center">Status</th>
+                <th>ACTION</th>
+              </tr>
+            </thead>
+            <tbody>
+              { 
+                tasks.map((task) => 
+                  <Task
+                    key={task.id}
+                    task={task}
+                    setModalEditOpen={(isOpen, task) => handleOpenEditModal(isOpen, task)}
+                    setModalDeleteOpen={(isOpen, id) => handleOpenDeleteModal(isOpen, id)}
+                  />)
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+
+      {/* Edit modal */}
+      <Modal
+        isShowModal={isShowEditModal}
+        setModalOpen={setIsShowEditModal}
+        >
+        <form onSubmit={handleSubmitEditTask}>
+          <h3 className='font-bold text-lg text-center text-primary'>
+            Edit task
+          </h3>
+
+          <div className='modal-action'>
+            <input
+              value={taskToEdit.title}
+              onChange={(e) => setTaskToEdit({ ...taskToEdit, title: e.target.value })}
+              type="text"
+              placeholder="New task"
+              className="input input-bordered w-full max-w-full text-black bg-white"
+            />
+
+            <button type='submit' className='btn btn-primary text-white'>
+              { isLoadingUpdate && <span className="loading loading-sm"></span> }
+              Update
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {/* Delete modal */}
+      <Modal
+        isShowModal={isShowDeleteModal}
+        setModalOpen={setIsShowDeleteModal}
+      >
+        <h3 className='font-bold text-lg text-center text-primary'>
+          Are you sure you want to delete this task?
+        </h3>
+
+        <div className="modal-action">
+          <div className="space-x-2 w-full flex">
+            <button
+              onClick={() => setIsShowDeleteModal(false)}
+              className="btn btn-ghost flex-1"
+            >
+              Cancel
+            </button>
+
+            <button
+              onClick={handleDeleteTask}
+              className="btn btn-error flex-1 text-white"
+            >
+              { isLoadingDelete && <span className="loading loading-sm"></span> }
+              Confirm
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </>
+    
   )
 }
 
