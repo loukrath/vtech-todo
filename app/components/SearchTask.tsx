26100d871a2848819a99c28005eabe13d40e47e6
munyrath <munyrath@gmail.com>
2023-12-21 23:05:34 +0700

    handle search after type 3 seconde

--- app/components/SearchTask.tsx
+++ app/components/SearchTask.tsx
@@ -1,9 +1,38 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const SearchTask = () => {
   const [searchVal, setSearchVal] = useState<string>('')
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null)
+
+  useEffect(() => {
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+      }
+    }
+  }, [timeoutId])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+
+    setSearchVal(value)
+
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    setTimeoutId(
+      setTimeout(() => {
+        handleSearch()
+      }, 3000)
+    )
+  }
+
+  const handleSearch = () => {
+    console.log('handleSearch: ', searchVal)
+  }
 
   return (
     <input
@@ -11,7 +40,7 @@ const SearchTask = () => {
       type="text"
       placeholder="Type to search..."
       className="input input-bordered w-full bg-white text-black flex-1"
-      onChange={(e) => setSearchVal(e.target.value)}
+      onChange={handleChange}
     />
   )
 }
